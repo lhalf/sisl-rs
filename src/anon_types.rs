e72b611d6239ad50dbcf168fb17484f7c73c1a43
Laurence <laurence.halfpenny@halfpenny.net>
2022-02-28 01:10:09 -0800

    update dumps to support i32-128 & u8-128

--- src/anon_types.rs
+++ src/anon_types.rs
@@ -16,7 +16,7 @@ impl Type for &str
     }
 }
 
-#[duplicate::duplicate_item(integer; [i8]; [i16])]
+#[duplicate::duplicate_item(integer; [i8]; [i16]; [i32]; [i64]; [i128]; [u8]; [u16]; [u32]; [u64]; [u128])]
 impl Type for integer
 {
     fn get_type(&self) -> String
--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -9,11 +9,47 @@ fn anon_string()
 #[test]
 fn anon_i8()
 {
-    assert_eq!("{\"_\": !_int \"1\"}", sisl::dumps(1 as i8));
+    assert_eq!("{\"_\": !_int \"-1\"}", sisl::dumps(-1 as i8));
 }
 
 #[test]
 fn anon_i16()
 {
-    assert_eq!("{\"_\": !_int \"32766\"}", sisl::dumps(32766 as i16));
+    assert_eq!("{\"_\": !_int \"-32768\"}", sisl::dumps(-32768 as i16));
+}
+
+#[test]
+fn anon_i32()
+{
+    assert_eq!("{\"_\": !_int \"-2147483648\"}", sisl::dumps(-2147483648 as i32));
+}
+
+#[test]
+fn anon_i64()
+{
+    assert_eq!("{\"_\": !_int \"-2147483649\"}", sisl::dumps(-2147483649 as i64));
+}
+
+#[test]
+fn anon_u8()
+{
+    assert_eq!("{\"_\": !_int \"1\"}", sisl::dumps(1 as u8));
+}
+
+#[test]
+fn anon_u16()
+{
+    assert_eq!("{\"_\": !_int \"65535\"}", sisl::dumps(65535 as u16));
+}
+
+#[test]
+fn anon_u32()
+{
+    assert_eq!("{\"_\": !_int \"4294967295\"}", sisl::dumps(4294967295 as u32));
+}
+
+#[test]
+fn anon_u64()
+{
+    assert_eq!("{\"_\": !_int \"4294967296\"}", sisl::dumps(4294967296 as u64));
 }
