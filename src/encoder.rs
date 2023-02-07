f3a6b8d2c3ccf67d1302ec525f586d5117a4129d
Laurence Halfpenny <laurence.halfpenny@paconsulting.com>
2023-02-07 09:50:20 +0000

    refactor to use format and proper SISL spec

--- src/encoder.rs
+++ src/encoder.rs
@@ -1,154 +1,179 @@
-pub fn dumps<T>(input: T) -> String where T: SISL
-{
-     String::from("{\"") + &input.get_name() + "\": !" + &input.get_type() + " \"" + &input.get_value() + "\"}"
+pub fn dumps<T: SISL>(input: T) -> String {
+    format!(
+        "{{\"{}\": !{} \"{}\"}}",
+        input.get_name(),
+        input.get_type(),
+        input.get_value()
+    )
 }
 
-pub trait SISL
-{
+pub trait SISL {
     fn get_name(&self) -> String;
     fn get_type(&self) -> String;
     fn get_value(&self) -> String;
 }
 
 use duplicate::duplicate_item;
-#[duplicate_item(basic_types; [i8]; [i16]; [i32]; [i64]; [u8]; [u16]; [u32]; [u64]; [f32]; [f64]; [&str]; [bool])]
-impl SISL for basic_types
-{
-    fn get_name(&self) -> String
-    {
+#[duplicate_item(int_types; [i8]; [i16]; [i32]; [i64]; [u8]; [u16]; [u32]; [u64])]
+impl SISL for int_types {
+    fn get_name(&self) -> String {
         String::from("_")
     }
-    fn get_type(&self) -> String
-    {
-        String::from("_") + &String::from(std::any::type_name::<basic_types>())
+    fn get_type(&self) -> String {
+        String::from("_int")
     }
-    fn get_value(&self) -> String
-    {
-        self.to_string().replace("\"", "")
+    fn get_value(&self) -> String {
+        self.to_string()
     }
 }
 
-#[duplicate_item(basic_types; [i8]; [i16]; [i32]; [i64]; [&str])]
-impl SISL for (&str, basic_types)
-{
-    fn get_name(&self) -> String
-    {
+#[duplicate_item(float_types; [f32]; [f64])]
+impl SISL for float_types {
+    fn get_name(&self) -> String {
+        String::from("_")
+    }
+    fn get_type(&self) -> String {
+        String::from("_float")
+    }
+    fn get_value(&self) -> String {
+        self.to_string()
+    }
+}
+
+impl SISL for &str {
+    fn get_name(&self) -> String {
+        String::from("_")
+    }
+    fn get_type(&self) -> String {
+        String::from("_str")
+    }
+    fn get_value(&self) -> String {
+        self.to_string()
+    }
+}
+
+impl SISL for bool {
+    fn get_name(&self) -> String {
+        String::from("_")
+    }
+    fn get_type(&self) -> String {
+        String::from("_bool")
+    }
+    fn get_value(&self) -> String {
+        self.to_string()
+    }
+}
+
+impl<T: SISL> SISL for (&str, T) {
+    fn get_name(&self) -> String {
         String::from(self.0)
     }
-    fn get_type(&self) -> String
-    {
-        String::from(std::any::type_name::<basic_types>())
+    fn get_type(&self) -> String {
+        self.1.get_type().replace("_", "")
     }
-    fn get_value(&self) -> String
-    {
-        self.1.to_string().replace("\"", "")
+    fn get_value(&self) -> String {
+        self.1.get_value()
     }
 }
 
 #[cfg(test)]
-mod tests
-{
+mod tests {
     use crate::dumps;
 
     #[test]
-    fn anon_str()
-    {
-    assert_eq!("{\"_\": !_&str \"test string\"}", dumps("test string"));
+    fn anon_str() {
+        assert_eq!("{\"_\": !_str \"test string\"}", dumps("test string"));
     }
 
     #[test]
-    fn anon_i8()
-    {
-    assert_eq!("{\"_\": !_i8 \"-1\"}", dumps(-1 as i8));
+    fn anon_i8() {
+        assert_eq!("{\"_\": !_int \"-1\"}", dumps(-1 as i8));
     }
 
     #[test]
-    fn anon_i16()
-    {
-    assert_eq!("{\"_\": !_i16 \"-32768\"}", dumps(-32768 as i16));
+    fn anon_i16() {
+        assert_eq!("{\"_\": !_int \"-32768\"}", dumps(-32768 as i16));
     }
 
     #[test]
-    fn anon_i32()
-    {
-    assert_eq!("{\"_\": !_i32 \"-2147483648\"}", dumps(-2147483648 as i32));
+    fn anon_i32() {
+        assert_eq!("{\"_\": !_int \"-2147483648\"}", dumps(-2147483648 as i32));
     }
 
     #[test]
-    fn anon_i64()
-    {
-    assert_eq!("{\"_\": !_i64 \"-2147483649\"}", dumps(-2147483649 as i64));
+    fn anon_i64() {
+        assert_eq!("{\"_\": !_int \"-2147483649\"}", dumps(-2147483649 as i64));
     }
 
     #[test]
-    fn anon_u8()
-    {
-    assert_eq!("{\"_\": !_u8 \"1\"}", dumps(1 as u8));
+    fn anon_u8() {
+        assert_eq!("{\"_\": !_int \"1\"}", dumps(1 as u8));
     }
 
     #[test]
-    fn anon_u16()
-    {
-    assert_eq!("{\"_\": !_u16 \"65535\"}", dumps(65535 as u16));
+    fn anon_u16() {
+        assert_eq!("{\"_\": !_int \"65535\"}", dumps(65535 as u16));
     }
 
     #[test]
-    fn anon_u32()
-    {
-    assert_eq!("{\"_\": !_u32 \"4294967295\"}", dumps(4294967295 as u32));
+    fn anon_u32() {
+        assert_eq!("{\"_\": !_int \"4294967295\"}", dumps(4294967295 as u32));
     }
 
     #[test]
-    fn anon_u64()
-    {
-    assert_eq!("{\"_\": !_u64 \"4294967296\"}", dumps(4294967296 as u64));
+    fn anon_u64() {
+        assert_eq!("{\"_\": !_int \"4294967296\"}", dumps(4294967296 as u64));
     }
 
     #[test]
-    fn anon_f32()
-    {
-    assert_eq!("{\"_\": !_f32 \"10.1\"}", dumps(10.1 as f32));
+    fn anon_f32() {
+        assert_eq!("{\"_\": !_float \"10.1\"}", dumps(10.1 as f32));
     }
 
     #[test]
-    fn anon_f64()
-    {
-    assert_eq!("{\"_\": !_f64 \"1000.1\"}", dumps(1000.1 as f64));
+    fn anon_f64() {
+        assert_eq!("{\"_\": !_float \"1000.1\"}", dumps(1000.1 as f64));
     }
 
     #[test]
-    fn anon_bool()
-    {
-    assert_eq!("{\"_\": !_bool \"true\"}", dumps(true));
+    fn anon_bool() {
+        assert_eq!("{\"_\": !_bool \"true\"}", dumps(true));
     }
 
     #[test]
-    fn str()
-    {
-        assert_eq!("{\"str name\": !&str \"test string\"}", dumps(("str name", "test string")))
+    fn str() {
+        assert_eq!(
+            "{\"str name\": !str \"test string\"}",
+            dumps(("str name", "test string"))
+        )
     }
 
     #[test]
-    fn i8()
-    {
-        assert_eq!("{\"i8 name\": !i8 \"-1\"}", dumps(("i8 name", -1 as i8)))
+    fn i8() {
+        assert_eq!("{\"i8 name\": !int \"-1\"}", dumps(("i8 name", -1 as i8)))
     }
 
     #[test]
-    fn i16()
-    {
-        assert_eq!("{\"i16 name\": !i16 \"-32768\"}", dumps(("i16 name", -32768 as i16)))
+    fn i16() {
+        assert_eq!(
+            "{\"i16 name\": !int \"-32768\"}",
+            dumps(("i16 name", -32768 as i16))
+        )
     }
 
     #[test]
-    fn i32()
-    {
-        assert_eq!("{\"i32 name\": !i32 \"-2147483648\"}", dumps(("i32 name", -2147483648 as i32)))
+    fn i32() {
+        assert_eq!(
+            "{\"i32 name\": !int \"-2147483648\"}",
+            dumps(("i32 name", -2147483648 as i32))
+        )
     }
 
     #[test]
-    fn i64()
-    {
-        assert_eq!("{\"i64 name\": !i64 \"-2147483649\"}", dumps(("i64 name", -2147483649 as i64)))
+    fn i64() {
+        assert_eq!(
+            "{\"i64 name\": !int \"-2147483649\"}",
+            dumps(("i64 name", -2147483649 as i64))
+        )
     }
 }
